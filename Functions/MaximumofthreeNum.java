19c0a382b0393c32024a54fefb75632f8770484e
SilEnT-24 <ak4689769@gmail.com>
2026-06-17 22:55:30 +0530

    Add functions to count digits, find maximum of three numbers, reverse a number, and check for prime numbers

---
+++ Functions/MaximumofthreeNum.java
@@ -0,0 +1,14 @@
+public class MaximumofthreeNum {
+    static int max3(int a, int b, int c){
+        if(a>=b && a>=c)
+            return a;
+        else if(b>=c)
+            return b;
+        else
+            return c;
+    }
+
+    public static void main(String[] args){
+        System.out.println(max3(10, 24, 3));
+    }
+}
