a865515dcf14cf63e7e86282b459a1c9c3ac518a
SilEnT-24 <ak4689769@gmail.com>
2026-07-05 19:52:09 +0530

    Refactor code structure for improved readability and maintainability

--- java's/Func's/CheckevenNumber.java
+++
@@ -1,9 +0,0 @@
-public class CheckevenNumber {
-    static boolean isEven(int n){
-        return n%2 == 0;
-    }
-
-    public static void main(String[] args){
-        System.out.println(isEven (8));
-    }
-}
