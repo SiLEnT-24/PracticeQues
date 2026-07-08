378805a5495f4df3a948e27dfbc0a5e691e771e8
SilEnT-24 <ak4689769@gmail.com>
2026-07-08 23:20:04 +0530

    Refactor InsertData1 class to improve structure and readability of data insertion logic

--- Java_Workshop/jdbc1/src/InsertData1.java
+++ Java_Workshop/jdbc1/src/InsertData1.java
@@ -1,3 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 public class InsertData1 {
-    
+    public static void main(String[] args) {
+
+        try {
+
+            // Load JDBC Driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Establish Connection
+            Connection c = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/alok",
+                    "root",
+                    "orion24"
+            );
+
+            PreparedStatement ps=c.prepareStatement(
+					"insert into student(name,phone)values(?,?)");
+			ps.setString(1,"Rohan");
+			ps.setString(2,"1122336655");
+			ps.executeUpdate();
+			System.out.println("Data Inserted!!");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+    }
 }
