8357c94d2a25a6cf8f52935c3af36d1a14f31bef
SilEnT-24 <ak4689769@gmail.com>
2026-07-08 23:12:10 +0530

    Implement data insertion in InsertData class and create ReadData class as a placeholder

--- Java_Workshop/jdbc1/src/InsertData.java
+++ Java_Workshop/jdbc1/src/InsertData.java
@@ -1,3 +1,31 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class InsertData {
+
+    public static void main(String[] args) {
+
+        try {
+
+            // Load JDBC Driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Establish Connection
+            Connection c = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/alok",
+                    "root",
+                    "orion24"
+            );
+
+			Statement st=c.createStatement();
+			st.executeUpdate(
+					"insert into student(name,phone)values('Alok','4561237893')");
+			System.out.println("Data Inserted!!");
+		}catch(Exception e) {
+			e.printStackTrace(); 
+		}
+
+    }
     
 }
