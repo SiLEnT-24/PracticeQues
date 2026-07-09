072c9b48dde801ff9806263484602a8ba74c4dc0
SilEnT-24 <ak4689769@gmail.com>
2026-07-09 21:38:50 +0530

    Add conceptofpackage class to demonstrate package usage

---
+++ Assignments/conceptofpackage.java
@@ -0,0 +1,8 @@
+// WAP to demonstrate the concept of package
+package Assignments;
+
+public class conceptofpackage {
+	public static void main(String[] args) {
+		System.out.println("Demonstrating package: Assignments");
+	}
+}
